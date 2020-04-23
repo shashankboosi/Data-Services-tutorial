e775ceefd782a09d604ec4e6fb5435e1201cce82
shashankboosi <shashank1872@gmail.com>
2020-04-24 04:28:23 +1000

    Update the file with feature extraction process

--- Tutorial-3/src/z5222766.py
+++ Tutorial-3/src/z5222766.py
@@ -1,10 +1,13 @@
 # The logic to the code is written by sboosi(z5222766)
 import sys
-
-import pandas as pd
-import numpy as np
 from ast import literal_eval
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from sklearn import linear_model
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 # output = pd.DataFrame(data={"id": X_test["id"], "Prediction": y_pred})
 # output.to_csv(path_or_buf="..\\output\\results.csv", index=False, quoting=3, sep=";")
@@ -16,7 +19,8 @@ def load_data(train_data, validation_data):
     return df_train, df_validation
 
 
-def data_drop(data):
+# Function to drop the columns which are not needed
+def data_drop(data, type):
     columns_to_drop = [
         "movie_id",
         "homepage",
@@ -24,34 +28,42 @@ def data_drop(data):
         "overview",
         "tagline",
         "spoken_languages",
-        "rating",
+        "original_title",
     ]
+
+    if type == "regression":
+        columns_to_drop.append("rating")
+    elif type == "classification":
+        columns_to_drop.append("revenue")
 
     return data.drop(columns_to_drop, axis=1)
 
 
 # Function to do the preliminary refinement of data
-def data_refine(data):
-    df_refine = data_drop(data)
+def data_refine(data, type):
+    df_refine = data_drop(data, type)
 
     # Convert  release date to date-time and also extract the year
     df_refine["release_date"] = pd.to_datetime(
-        df_refine["release_date"], format="%Y-%m-%d"
+        df_refine["release_date"], format="%Y-%m-%d", errors="coerce"
     )
+
     df_refine["release_year"] = df_refine["release_date"].dt.year
+    df_refine["release_month"] = df_refine["release_date"].dt.month
+    df_refine.drop("release_date", axis=1, inplace=True)
 
     return df_refine
 
 
 # This function extracts the first 3 attributes tag from the json feature objects and group them into a list of strings
-def extract_attributes_from_json(json_data, attribute_name):
+def extract_attributes_from_json(json_data, attribute_name, no_of_elements):
     json_data = literal_eval(json_data)
     if json_data == []:
         return np.nan
     result_list = []
     count = 0
     for row in json_data:
-        if count < 3:
+        if count < no_of_elements:
             result_list.append(row[attribute_name])
             count += 1
         else:
@@ -59,32 +71,109 @@ def extract_attributes_from_json(json_data, attribute_name):
     return result_list
 
 
+# This function extracts the job attributes with value Director from the json feature objects
+def get_director_from_crew(json_data):
+    json_data = literal_eval(json_data)
+    if json_data == []:
+        return np.nan
+    for row in json_data:
+        if row["job"] == "Director":
+            return row["name"]
+    return np.nan
+
+
 # Function to extract the data out of the json object
-def data_extraction(data):
-    df_clean = data_refine(data)
+def data_clean(data, type):
+    df_clean = data_refine(data, type)
 
-    # features = [
-    #     ["cast", "name"],
-    #     "keywords",
-    #     "genres",
-    #     "production_companies",
-    #     "production_countries",
-    # ]
-    # for feature in features:
-    #     df_clean[feature] = df_clean[feature].apply(literal_eval)
+    # Features and their attributes with no of elements
+    features = [
+        ["cast", "name", 3],
+        ["keywords", "name", 3],
+        ["genres", "name", 3],
+        ["production_companies", "name", 3],
+        ["production_countries", "iso_3166_1", 3],
+    ]
+    # Extract the mentioned attributes from given feature
+    for feature in features:
+        df_clean[feature[0]] = df_clean[feature[0]].apply(
+            lambda row: extract_attributes_from_json(row, feature[1], feature[2])
+        )
 
-    print(df_clean.iloc[1])
-    # Extract the name attribute from cast feature
-    df_clean["cast"] = df_clean["cast"].apply(
-        lambda row: extract_attributes_from_json(row, "name")
+    # Extract the director attributes from crew feature
+    df_clean["director"] = df_clean["crew"].apply(get_director_from_crew)
+    df_clean.drop("crew", axis=1, inplace=True)
+    df_clean.dropna(inplace=True)
+
+    return df_clean
+
+
+def replace_attribute_with_target_mean_values(data, value_dict, target):
+    raw_dict = value_dict[target]
+    if isinstance(data, list):
+        result_list = []
+        for i in data:
+            result_list.append(raw_dict[i])
+        return int(sum(result_list) / len(result_list))
+    elif isinstance(data, str):
+        return int(raw_dict[data])
+
+
+def convert_the_attributes_based_on_target(data, feature, target):
+    conversion_result = {}
+    if isinstance(data[feature].iloc[0], list):
+        data_stack = pd.DataFrame(
+            {
+                col: np.repeat(data[col].values, data[feature].str.len())
+                for col in data.columns.drop(feature)
+            }
+        ).assign(**{feature: np.concatenate(data[feature].values)})[data.columns]
+        conversion_result = data_stack.groupby([feature]).mean().to_dict()
+    elif isinstance(data[feature].iloc[0], str):
+        conversion_result = data.groupby([feature]).mean().to_dict()
+
+    return data[feature].apply(
+        lambda row: replace_attribute_with_target_mean_values(
+            row, conversion_result, target
+        )
     )
-    print(df_clean["cast"].iloc[1])
-    # print(df_clean.columns)
-    print(len(df_clean))
 
 
-def data_clean(data):
-    data_extraction(data)
+def string_transformation(string_data):
+    if isinstance(string_data, list):
+        return [str.lower(i.replace(" ", "")) for i in string_data]
+    elif isinstance(string_data, str):
+        return str.lower(string_data.replace(" ", ""))
+
+
+def feature_extraction_and_transformation(clean_data, target):
+    features = [
+        "cast",
+        "keywords",
+        "genres",
+        "production_companies",
+        "production_countries",
+        "director",
+    ]
+
+    for feature in features:
+        clean_data[feature] = clean_data[feature].apply(string_transformation)
+        df_feature = clean_data[[feature, target]].copy()
+        clean_data[feature + "_new"] = convert_the_attributes_based_on_target(
+            df_feature, feature, target
+        )
+        clean_data.drop(feature, axis=1, inplace=True)
+
+    le = LabelEncoder()
+    clean_data["original_language"] = le.fit_transform(clean_data["original_language"])
+
+    print(clean_data.iloc[1])
+
+    min_max = MinMaxScaler()
+    for i in clean_data.columns:
+        clean_data[i] = min_max.fit_transform(clean_data[i]).reshape(1, -1)
+
+    print(clean_data.iloc[1])
 
 
 if __name__ == "__main__":
@@ -95,11 +184,13 @@ if __name__ == "__main__":
         argument_list = full_cmd_arguments[1:]
         # Send the file names coming from command prompt
         train, validation = load_data(argument_list[0], argument_list[1])
-        data_clean(train)
+        df_train_clean = data_clean(train, "regression")
+        feature_extraction_and_transformation(df_train_clean, "revenue")
         print()
         print("---------------Validation---------------")
         print()
-        data_clean(validation)
+        df_clean_val = data_clean(validation, "regression")
+        feature_extraction_and_transformation(df_clean_val, "revenue")
 
     else:
         raise Exception(
